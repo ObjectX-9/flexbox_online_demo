b116055f8b2b38a45965dea2723eafde127b18a6
zhuling822 <ling.zhu@js.design>
2024-05-16 10:56:26 +0800

    🐛fix(custom): readme.md

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,10 @@ import './app.scss';
 function App() {
 	return (
 		<>
+			{/* 头部 */}
+			{/* 布局展示区域 */}
+			{/* 配置区域 */}
+			{/* 底部 */}
 			<Header></Header>
 			<div className='App'>React18 + Ts5 + webpack5 开发模板搭建</div>;
 			<img src={img}></img>
